2e64f244b20757dc02194d128eadef7db2c745fb
bardin08 <asdasfb326@gmail.com>
2021-02-15 20:00:14 +0200

    Add domain models
    Created book, user, and keyword models. Also created enum representation for a user role

---
+++ CourseWork.Domain/Models/Enums/UserRole.cs
@@ -0,0 +1,18 @@
+﻿namespace CourseWork.Domain.Models.Enums
+{
+    public enum UserRole
+    {
+        /// <summary>
+        /// Guest user role representation
+        /// </summary>
+        Guest,
+        /// <summary>
+        /// Administrator user role representation
+        /// </summary>
+        Administrator,
+        /// <summary>
+        /// Author user role representation
+        /// </summary>
+        Author
+    }
+}
---
+++ CourseWork.Domain/Models/UserModel.cs
@@ -0,0 +1,30 @@
+﻿using CourseWork.Domain.Models.Enums;
+
+namespace CourseWork.Domain.Models
+{
+    public class UserModel
+    {
+        /// <summary>
+        /// User model unique identifier 
+        /// </summary>
+        /// <remarks>
+        /// Represented by a GUID in a string view
+        /// </remarks>
+        public string Id { get; set; }
+        /// <summary>
+        /// User first name
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// User last name
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// User role
+        /// </summary>
+        /// <remarks>
+        /// Represented by a <see cref="UserRole"/>
+        /// </remarks>
+        public UserRole Role { get; set; }
+    }
+}
