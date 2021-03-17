cbd518c70ce71379cd1c5238194a46fceba204f8
bardin08 <asdasfb326@gmail.com>
2021-03-17 08:58:13 +0200

    Add factory to create searching strategies

---
+++ CourseWork.LogicLayer/Abstractions/IBookSearchingStrategyFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CourseWork.Domain.Dtos;
+
+namespace CourseWork.LogicLayer.Abstractions
+{
+    internal interface IBookSearchingStrategyFactory
+    {
+        IEnumerable<IBookSearchingStrategy> GetSearchingStrategies(BookSearchingDto bookSearchingDto);
+    }
+}
---
+++ CourseWork.LogicLayer/Factories/DefaultBookSearchingStrategyFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CourseWork.Domain.Dtos;
+using CourseWork.LogicLayer.Abstractions;
+using CourseWork.LogicLayer.Strategies.BookSearchingStrategies;
+
+namespace CourseWork.LogicLayer.Factories
+{
+    internal class DefaultBookSearchingStrategyFactory : IBookSearchingStrategyFactory
+    {
+        public IEnumerable<IBookSearchingStrategy> GetSearchingStrategies(BookSearchingDto bookSearchingDto)
+        {
+            var searchingStrategies = new List<IBookSearchingStrategy>(bookSearchingDto.SearchingCriteriaAmount);
+            
+            if (bookSearchingDto.SearchByIsbn) searchingStrategies.Add(new SearchBookByIsbnStrategy()); 
+            if (bookSearchingDto.SearchByName) searchingStrategies.Add(new SearchBookByNameStrategy()); 
+            if (bookSearchingDto.SearchByPublishYear) searchingStrategies.Add(new SearchBookByPublishYearStrategy());
+            if (bookSearchingDto.SearchByAuthor) searchingStrategies.Add(new SearchBookByAuthorStrategy()); 
+            if (bookSearchingDto.SearchByKeyWords) searchingStrategies.Add(new SearchBookByKeyWordStrategy());
+
+            return searchingStrategies;
+        }
+    }
+}
