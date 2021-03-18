d7e8bbabded06f4eb7975992347323a735542141
bardin08 <asdasfb326@gmail.com>
2021-03-18 11:04:17 +0200

    Update UI

--- CourseWork.UI/Startup.cs
+++ CourseWork.UI/Startup.cs
@@ -1,5 +1,6 @@
 using CourseWork.Data;
 using CourseWork.Data.Contexts;
+using CourseWork.LogicLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -8,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CourseWork.UI.Areas.Identity;
+using CourseWork.UI.Helpers;
 
 namespace CourseWork.UI
 {
@@ -23,15 +25,24 @@ namespace CourseWork.UI
         public void ConfigureServices(IServiceCollection services)
         {
             services.UseDataAccessLayer(Configuration);
+            services.UseLogicalLayer();
             
             services.AddDefaultIdentity<IdentityUser>(options => 
-                options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationIdentityDbContext>();
+                options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationIdentityDbContext>();
 
             services.AddRazorPages();
             services.AddServerSideBlazor();
             
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddDatabaseDeveloperPageExceptionFilter();
+            
+            services.AddAuthorization(config =>
+            {
+                config.AddPolicy(Policies.IsAdmin, Policies.IsAdminPolicy());
+                config.AddPolicy(Policies.IsUser, Policies.IsUserPolicy());
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
