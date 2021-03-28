23abba915965f11bc1652b8659dc53c79d3aaa43
bardin08 <asdasfb326@gmail.com>
2021-03-28 21:11:56 +0300

    Add configuration project

---
+++ CourseWork.Configuration/DependencyInjection/DependencyInjectionConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using CourseWork.Data;
+using CourseWork.Data.Contexts;
+using CourseWork.LogicLayer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CourseWork.Configuration.DependencyInjection
+{
+    public static class DependencyInjectionConfiguration
+    {
+        public static void AddDataAccessLayer(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.UseDataAccessLayer(configuration);
+        }
+
+        public static void AddLogicLayer(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.UseLogicLayer();
+        }
+
+        public static void AddIdentity(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDefaultIdentity<IdentityUser>(options => 
+                    options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+        }
+    }
+}
